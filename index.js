2e09cbd76bb2cdc4f0b968ad4374f38113ca7123
Apipol Niyomsak <makemek101@hotmail.com>
2017-03-27 22:02:06 +0700

    add linting rules

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+module.exports = {
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'brace-style': [
+      'error',
+      '1tbs'
+    ],
+    'curly': [
+      'error',
+      'multi',
+      'consistent'
+    ],
+    'comma-spacing': [
+      'error',
+      {'before': false, 'after': true}
+    ],
+    'strict': [
+      'error',
+      'global'
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed'
+    ],
+    'arrow-spacing': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-spread': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'dot-notation': 'error',
+    'no-trailing-spaces': 'error',
+    'no-tabs': 'error',
+    'eol-last': 'error'
+  }
+}
